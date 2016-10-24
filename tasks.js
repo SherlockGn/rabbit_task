2173a363bc741278ec13dc354125adcc630cfe0b
Tianhe Gong (RD-CN-INTRN) <tianhe_gong@tw.trendnet.org>
2016-10-24 12:57:16 +0800

    finish task list

---
+++ tasks.js
@@ -0,0 +1,50 @@
+var tasks = [
+{
+    name: "finish the practice of SQL",
+    etime: "2016-10-24",
+    ftime: false,
+    link: "problem1.html"
+},
+{
+    name: "finish 2 problems of Leetcode",
+    etime: "2016-10-24",
+    ftime: false,
+    remark: "not finish the task yet"
+},
+{
+    name: "finish 2 problems of Leetcode",
+    etime: "2016-10-25",
+    ftime: false,
+    remark: "not finish the task yet"
+},
+{
+    name: "finish 2 problems of Leetcode",
+    etime: "2016-10-26",
+    ftime: false,
+    remark: "not finish the task yet"
+},
+{
+    name: "finish 2 problems of Leetcode",
+    etime: "2016-10-27",
+    ftime: false,
+    remark: "not finish the task yet"
+},
+{
+    name: "finish 2 problems of Leetcode",
+    etime: "2016-10-28",
+    ftime: false,
+    remark: "not finish the task yet"
+},
+{
+    name: "finish 2 problems of Leetcode",
+    etime: "2016-10-29",
+    ftime: false,
+    remark: "not finish the task yet"
+},
+{
+    name: "finish 2 problems of Leetcode",
+    etime: "2016-10-30",
+    ftime: false,
+    remark: "not finish the task yet"
+}
+];
